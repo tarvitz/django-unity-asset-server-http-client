c49148792ffee7e950bb9e49667c1b4440b068d0
Tarvitz <tarvitz@blacklibrary.ru>
2014-10-31 12:57:38 +0300

    add version 0.1.2

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-unity-asset-server-http-client',
-    version='0.1.1',
+    version='0.1.2',
     install_requires = ['django-filter>=0.8',
                         'djangorestframework>=2.4.3'],
     packages=['duashttp'],
